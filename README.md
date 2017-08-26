dd1e8dea699891d14a6c8e8f439e25c7f720977e
Madhav Malpani <mm305@snu.edu.in>
2017-08-26 19:58:12 +0100

    comments added

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 
-## Download files from URL
+## Download static files from URL
 A script written in python to download file(s) from any URL
 
 ### usage:
    ```temp.py [-h] -u <URL> [-l <location>] [-e <ext> [<ext> ...]]```
 
-### arguments: 
-  
+### arguments:
+
     -u URL              Target URL   
     -l location         Save location | default : dnlwd   
     -e ext [ext ...]  extention(s)
-    -h, --help          show this help message and exit 
+    -h, --help          show this help message and exit
--- temp.py
+++ temp.py
@@ -1,23 +1,48 @@
-import sys, urllib, re, urlparse, os
-from bs4 import BeautifulSoup
+import sys, urllib, re, urlparse, os, argparse
+# using BeautifulSoup to scrape download links
+from bs4 import BeautifulSoup as bs
 
-if not len(sys.argv) == 3:
-    print >> sys.stderr, "Usage: %s <URL> <directory>" % (sys.argv[0],)
-    sys.exit(1)
 
-url = sys.argv[1]
-fol = os.path.join(os.getcwd(),sys.argv[2])
-f = urllib.urlopen(url)
+parser = argparse.ArgumentParser(description='Download static files from any URL')
 
-soup = BeautifulSoup(f,"lxml")
+parser.add_argument('-u',metavar="<URL>",help="Target URL",required=True)
+parser.add_argument('-l',metavar="<location>",help="Save location | default : ./dnlwd",default="dnwld")
+parser.add_argument('-e',metavar="<ext>",help="comma seperated list of arguments",default='pdf')
 
-if not os.path.exists(fol):
-  os.makedirs(fol)
-os.chdir(fol)
+#parsing and setting up parameters
+args = parser.parse_args()
+ext = '|'.join(args.e.split(","))
 
-for i in soup.findAll('a', attrs={'href': re.compile('(?i)(pdf)$')}):
-    full_url = urlparse.urljoin(url, i['href'])
-    urllib.urlretrieve(full_url,i.text)
-    print 'working\t',i.text
+"""
+url: target URL
+fol: dir to store downloaded items
+ext: prefered extension(s) to download(not implemented yet)
+"""
+def start(url,fol,ext=None):
+	print (url)
+	try:
+		f = urllib.urlopen(url)
+		soup = bs(f,"lxml")
+	except:
+		print "Bad URL:\t" + url
+		parser.print_help()
+		sys.exit(1)
 
-print "Done"
+#creating fol dir if it doesn't exist
+	if not os.path.exists(fol):
+		os.makedirs(fol)
+
+	os.chdir(fol)
+	expr = '(?i)('+ext+')$'
+	print (expr)
+	for i in soup.findAll('a', attrs={'href': re.compile(expr)}):
+		link_url = urlparse.urljoin(url, i['href'])
+
+		print 'working\t',i.text
+		urllib.urlretrieve(link_url,i.text)
+
+	print "done"
+	return
+
+#calling function with all parameters set
+start(args.u,args.l,ext)
